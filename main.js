cf21dd07bf2ec89195ba17620d048f76d93c583e
Camila Carvalho <carvalho.camiilaa@gmail.com>
2023-01-30 13:33:09 -0300

    v-binding

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      description: "Beautiful e confortble",
+      image: "./assets/images/socks_green.jpg",
     };
   },
 });
